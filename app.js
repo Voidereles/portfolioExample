cc7f2e4e3281834bf6ae46ca39be1a5e2d9a33b9
Maciej <maciej.zelazo@protonmail.com>
2021-01-11 00:32:10 +0100

    build main page

---
+++ app.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import 'owl.carousel';
+import 'bootstrap';
+import './scripts/owlScripts.js';
+
+// document.getElementById('#welcomeVideo').playbackRate = 0.5;
+document.querySelector('.welcome video').playbackRate = 0.8;
+// document.getElementById('#welcomeVideo').play();
