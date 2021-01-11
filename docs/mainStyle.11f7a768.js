a11cd095d0a6e55b37062ee78ab7728da4c96a50
Maciej <maciej.zelazo@protonmail.com>
2021-01-11 02:27:36 +0100

    added letter icon

--- docs/mainStyle.11f7a768.js
+++ docs/mainStyle.11f7a768.js
@@ -235,7 +235,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39917" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38665" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
